09fca61d8d70bab576d83a9e7d99f1ca5825ff5f
Pietro Verdile <pietro.verdile@visto.global>
2021-09-14 23:19:05 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
