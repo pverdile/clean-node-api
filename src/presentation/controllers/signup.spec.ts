04d6f68f917b475143470efb39dadaa6b8b118db
Pietro Verdile <pietro.verdile@visto.global>
2021-09-15 00:47:44 -0300

    test: ensure SignUpController implement correct interfaces

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,21 @@
 import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
+  test('should return 200 if body is ok', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toBe('love')
+  })
+
   test('should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
